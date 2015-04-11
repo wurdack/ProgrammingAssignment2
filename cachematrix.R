3d260a5e777d08086a6afe2993318047efa69823
Richard Wurdack <rwurdack@jawbone.com>
2015-04-10 18:42:21 -0700

    Implementation of cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Routines to support caching of matrix inversion.
 
-## Write a short comment describing this function
-
+## Description: This function creates a wrapper object around a matrix. The
+##              wrapper object caches the inverse of the matrix
+##
 makeCacheMatrix <- function(x = matrix()) {
 
+        ## Inverse is initially empty
+        ##
+        i <- NULL
+
+        ## Routine to set the matrix. Invalidates the cached content.
+        ##
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+
+        ## Routine to recover the matrix
+        ##
+        get <- function() x
+
+        ## Routine to cache the inverse
+        ##
+        setinv <- function(inv) i <<- inv
+
+        ## Routine to recover the cached inverse
+        ##
+        getinv <- function() i
+
+        ## Return a list with the routines for setting/getting content and
+        ## cached matrix inverse.
+        ##
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Description: This function returns the inverse of a cache matrix. If the
+##              inverse has not been calculated, it will calculate it and store
+##              it in the cache. Otherwise it simply returns the cached content.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        ## Recover the cached content.
+        ##
+        i <- x$getinv()
+
+        ## If we do not have a cached copy, calculate the inverse now.
+        ##
+        if (is.null(i)) {
+                message("Creating cached inverse")
+
+                ## Recover matrix
+                m <- x$get()
+
+                ## Get the inverse, using the caller supplied options.
+                i <- solve(m, ...)
+
+                ## Cache the inverst
+                x$setinv(i)
+
+        } else {
+                message("Returning cached copy")
+        }
+
+        i
 }
